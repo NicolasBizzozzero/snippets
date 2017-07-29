f7192492703705a4457f14148533d41a466cd056
NicolasBi <nicolasbizzozzero@gmail.com>
2017-07-29 22:13:06 +0200

    Merged content from T100HA

--- c/nbu_string.c
+++ c/nbu_string.c
@@ -2,6 +2,34 @@
 #include <malloc.h>
 #include "nbu_string.h"
 
+/**
+ * Return the number in str.
+ */
+unsigned int len(char* str);
+
+/**
+ * Create and return a copy of str.
+ */
+char* copy(char* str);
+
+/**
+ * Return a reversed copy of str
+ */
+char* reverse(char* str);
+
+/**
+ * Return the number of time c appear in str.
+ */
+unsigned int count(char* str, char c);
+
+/**
+ * Return the number of time substr appear in str.
+ */
+unsigned int count_substr(char* str, char* substr);
+
+void str_free(char* str);
+
+
 unsigned int len(char* str) {
     if (str == NULL)
         return 0;
