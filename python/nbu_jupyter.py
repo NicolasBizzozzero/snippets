ecaee7fb3ce67eed39a964fd7099d39faa8464f8
Bizzozzéro Nicolas <nicolasbizzozzero@gmail.com>
2018-10-13 21:08:43 +0200

    Update nbu_jupyter.py

--- python/nbu_jupyter.py
+++ python/nbu_jupyter.py
@@ -29,13 +29,20 @@ class PDF(object):
         return r"\includegraphics[width=1.0\textwidth]{{{0}}}".format(self.path_to_pdf)
 
     
-def plot_correlation_matrix(df, size=8, cmap="plasma", show=True):
+def plot_correlation_matrix(df, size=8, cmap="plasma", masked=False, show=True):
     """ Plot a graphical correlation matrix for each pair of columns in the dataframe.
     
     :param df: pandas DataFrame.
     :param size: Vertical and horizontal size of the plot.
+    :param cmap: The ColorMap used to display the result. See : https://matplotlib.org/tutorials/colors/colormaps.html
+    :param masked: Mask the upper diagonal of the heatmap.
+    :param show: Show the result plotted.
     """
     corr = df.corr()
+    
+    if masked:
+        corr[get_diagonal_mask(corr.values)] = np.nan
+    
     fig, ax = plt.subplots(figsize=(size + 1, size))
     img = ax.matshow(corr, cmap=cmap)
     fig.colorbar(img, ax=ax)
@@ -46,3 +53,14 @@ def plot_correlation_matrix(df, size=8, cmap="plasma", show=True):
 
     if show:
         plt.show()
+
+
+def get_diagonal_mask(data: np.ndarray) -> np.ndarray:
+    """ Return a diagonal mask computed from an array.
+    Useful when the data is the same if you transpose the array, eg in a heatmap.
+    
+    :param data: The np.ndarray from which you want to compute the mask.
+    """
+    mask = np.zeros_like(data, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+    return mask
